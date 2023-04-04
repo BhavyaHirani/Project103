e22db2caf790091a265dacacaaf2be404e813aa1
BhavyaHirani <109067012+BhavyaHirani@users.noreply.github.com>
2023-04-04 14:08:34 +0200

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,38 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/Navin/Desktop/"
+
+class fileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Hey, {event.src_path} has been created!")
+
+    def on_deleted(self, event):
+        print(f"Oops! Someone deleted {event.src_path}")
+
+    def on_modified(self, event):
+        print(f"Hey, {event.src_path} fas been modified") 
+
+    def on_moved(self, event):
+        print(f"Oops! Someone has moved {event.src_path}")        
+
+event_handler = fileEventHandler()
+
+observer = Observer()
+
+observer.schedule(event_handler, from_dir, recursive=True)
+
+observer.start()
+
+try: 
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped!")
+    Observer.stop()                
